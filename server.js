3f7b70ff85921739a2e8bbde7e05ec37bacfc50f
Asagi-Ai-Git <myrpextraemail-github@yahoo.com>
2019-10-08 00:26:52 -0400

    using your own function

--- server.js
+++ server.js
@@ -2,10 +2,15 @@
 var http = require('http');
 var port = process.env.PORT || 1337;
 
-var name = "ryan";
-
+/*
 http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World \n' + name);
+   
 }).listen(port);
+*/
 
+http.createServer(my_website(req, res)).listen(port);
+
+function my_website(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World \n');
+}
