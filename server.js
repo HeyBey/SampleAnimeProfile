efbe0dd4712fce978e96ec6ae490e6c4d9338180
Asagi-Ai-Git <myrpextraemail-github@yahoo.com>
2019-10-08 00:52:55 -0400

    code testing

--- server.js
+++ server.js
@@ -11,10 +11,12 @@ http.createServer(function (req, res) {
 http.createServer(my_website(req, res)).listen(port);
 
 function my_website(req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World \n');
-
-    if(req.url == "/test") {
+   
+    if (req.url == "/") {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Hello World \n');
+    }
+    else if (req.url == "/error") {
         res.writeHead(404, { 'Content-Type': 'text/html' });
         res.end("Sorry Your Page is Unavailable");
     }
