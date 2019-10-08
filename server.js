b25b8b70689573deef6e5b3b17f54ec516ed83f3
Asagi-Ai-Git <myrpextraemail-github@yahoo.com>
2019-10-08 00:46:30 -0400

    code testing

--- server.js
+++ server.js
@@ -14,6 +14,9 @@ function my_website(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World \n');
 
-    res.writeHead(404, { 'Content-Type': 'text/html' });
-    res.end("Sorry Your Page is Unavailable");
+    if(req.url == "/test" && req.method == "GET") {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.end("Sorry Your Page is Unavailable");
+    }
+  
 }
