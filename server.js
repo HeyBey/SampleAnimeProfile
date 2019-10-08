ebf4528e9fdaf45e478d27ff5d1f01c1be5afc7a
Asagi-Ai-Git <myrpextraemail-github@yahoo.com>
2019-10-08 00:17:18 -0400

    naming ryan

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-'use strict';
+//'use strict';
 var http = require('http');
 var port = process.env.PORT || 1337;
 
+var name = "ryan";
+
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World 22\n');
+    res.end('Hello World \n' + name);
 }).listen(port);
+
