18e32f244ed2eccd3750880b823d3a5d4bbadd77
Asagi-Ai-Git <myrpextraemail-github@yahoo.com>
2019-10-08 01:48:28 -0400

    code testing

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ function my_website(req, res) {
     }
     else if (req.url == "/sample" && req.method == "GET") {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('Hello World Sample \n' + req.url);
+        res.write('Hello World Sample \n' + req.url);
     }
     else {
         res.writeHead(404, { 'Content-Type': 'text/plain' });
